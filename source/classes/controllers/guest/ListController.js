0c7092b73765581e89c3f0d6858b7726ebecf06d
Dominik Thamm <dom.tam705@gmail.com>
2015-12-09 10:58:14 +0100

    do some more stuff

--- source/classes/controllers/guest/ListController.js
+++ source/classes/controllers/guest/ListController.js
@@ -2,6 +2,7 @@ define(['app/models/GuestRepository'], function(GuestRepository) {
     'use strict';
     var GuestListController = function($scope, $routeParams, GuestRepository) {
         this.scope = $scope;
+        this.scope.event = { id:$routeParams.eventId };
         GuestRepository.forEvent({ id:$routeParams.eventId },
             function(guests) {
                 this.scope.guests = guests;
